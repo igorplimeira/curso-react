1199f64766a4db575ca4bdd8fb240bd77ebb167f
igorpaz <igorpaz@fortestecnologia.com.br>
2021-10-27 15:14:51 -0300

    Aula 12 Criação de Componentes

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import react from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    nome : ''
+  }
+
+/*
+
+  constructor(){
+    super()
+    this.modificarNome = this.modificarNome.bind(this)
+  }
+  modificarNome(event){
+    this.setState({
+      nome: event.target.value
+    })
+  }
+*/
+
+  modificarNome = (event) => {
+    this.setState({
+      nome: event.target.value
+    })
+  }
+
+  criaComboBox = () => {
+    const opcoes = ["Igor", "Stephanny", "Tânia"]
+    const comboBoxOpcoes = opcoes.map (opcao => <option>{opcao}</option> )
+
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
+  render(){
+
+    const MeuComboBox = () => this.criaComboBox()
+
+    return (
+      <react.Fragment>
+        <input type="text" value={this.state.nome} onChange={this.modificarNome} />
+        <h1>{this.state.nome}</h1>
+        <MeuComboBox/>
+      </react.Fragment>
+    )
+  }
 }
 
 export default App;
